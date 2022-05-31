c7ec74216fc74ba1a3c1f6d47bf95acac0c16c3d
Kirill <freshjs.ftd.@gmail.com>
2022-05-31 19:08:21 +0300

    Remove anonymous default export from constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 const serverIP = 'localhost';
 const serverPort = 5000;
-export default {
+const CONSTANTS = {
   CUSTOMER: 'customer',
   CREATOR: 'creator',
   CONTEST_STATUS_ACTIVE: 'active',
@@ -78,3 +78,5 @@ export default {
     },
   ],
 };
+
+export default CONSTANTS;
